9b824a843d1f3645ba2a11eba5e9792d312ae9e6
MithoonKumar <kumarmithoon6@gmail.com>
2020-01-08 19:42:30 +0530

    Use scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text("Hello Mithoon")
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('my second app'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text("Hello Followers"),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text("Click"),
+    ),
+  )
 ));
